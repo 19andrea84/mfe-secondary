02c885e58555a965b95a9d866b1e5bf0f878ddff
a.delentinis <a.delentinis@eu.aswatson.com>
2026-01-15 16:27:28 +0100

    adjustment

---
+++ app/checkout/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+  return (
+    <div>Ciao Andrea, checkout service is also running</div>
+  );
+}
+
---
+++ app/secondary/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div>Ciao Andrea, secondary service is also running</div>
+  );
+}
